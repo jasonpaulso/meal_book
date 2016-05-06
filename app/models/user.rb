f9af5dd42c6a648f9367117aa129840eda332f29
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-06 14:12:26 -0700

    done...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, message: "You must create a username"
-  validates :username, uniqueness: { case_sensitive: true,  message: "This username has already been taken." }
+  validates :username, uniqueness: { case_sensitive: false,  message: "This username has already been taken." }
   validates_presence_of :password, message: "You must create a password"
   has_secure_password validations: false
   has_many :meals
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_by_slug(slug)
-    User.all.find {|user| user.slug == slug}
+    User.all.find {|user| user.slug == slug.downcase}
   end
 
 end
